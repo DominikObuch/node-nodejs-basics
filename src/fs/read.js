1f835c84a4686296308a6e2c346f2c2d16d677ab
Dominik Obuch <dominik.obuch@wp.pl>
2025-10-26 15:42:24 +0100

    feat: fs/read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
 const read = async () => {
-  // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+  try {
+    const data = await fs.readFile(filePath, 'utf8');
+    console.log(data);
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();
