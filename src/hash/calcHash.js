d547f0d951e7f7da289bbd2c3405905d7a237922
Dominik Obuch <dominik.obuch@wp.pl>
2025-10-26 16:40:32 +0100

    feat: hash/calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import fs from 'fs';
+import crypto from 'crypto';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-  // Write your code here
+  const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  const hash = crypto.createHash('sha256');
+  const readStream = fs.createReadStream(filePath);
+
+  readStream.on('data', (chunk) => {
+    hash.update(chunk);
+  });
+
+  readStream.on('end', () => {
+    console.log(hash.digest('hex'));
+  });
+
+  readStream.on('error', (err) => {
+    throw new Error('Hash calculation failed');
+  });
 };
 
 await calculateHash();
